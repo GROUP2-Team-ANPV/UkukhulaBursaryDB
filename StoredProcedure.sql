73ba376185078525436aed2c4d324e7d9baa3c94
alfred-malope <malopealfred12@gmail.com>
2024-02-23 10:58:22 +0200

    university stored procedure

---
+++ StoredProcedure.sql
@@ -0,0 +1,44 @@
+CREATE PROCEDURE AddUniverityAndUser
+    @UniversityName VARCHAR(120),
+    @ProvinceID INT,
+    @FirstName VARCHAR(120),
+    @LastName VARCHAR(120),
+    @Email VARCHAR(255),
+    @PhoneNumber VARCHAR(13),
+    @RoleID INT,
+    @DepartmentID INT
+AS
+BEGIN
+    BEGIN TRY
+        BEGIN TRANSACTION;
+        
+        DECLARE @UniversityID INT;
+        DECLARE @ContactID INT;
+        DECLARE @UserID INT;
+
+        INSERT INTO [dbo].[University] ([Name], [ProvinceID])
+        VALUES (@UniversityName, @ProvinceID);
+
+        SET @UniversityID = SCOPE_IDENTITY(); 
+
+        INSERT INTO [dbo].[ContactDetails] ([Email], [PhoneNumber])
+        VALUES (@Email, @PhoneNumber);
+
+        SET @ContactID = SCOPE_IDENTITY(); 
+
+        INSERT INTO [dbo].[User] ([FirstName], [LastName], [ContactID], [RoleID])
+        VALUES (@FirstName, @LastName, @ContactID, @RoleID);
+
+        SET @UserID = SCOPE_IDENTITY(); 
+
+        INSERT INTO [dbo].[UniversityUser] ([UniversityID], [UserID], [DepartmentID])
+        VALUES (@UniversityID, @UserID, @DepartmentID);
+
+        COMMIT TRANSACTION;
+    END TRY
+    BEGIN CATCH
+        IF @@TRANCOUNT > 0
+            ROLLBACK TRANSACTION;
+		RETURN error_message();
+    END CATCH;
+END;
